4c489c1c9ade09d48e92897ede68b7f27d654901
abdou6666 <abdousfayhi12@gmail.com>
2023-10-18 11:17:27 +0100

    seance 6 (keycloak & security)

--- Candidate/src/main/java/com/esprit/microservice/candidateservice/CandidatRestAPI.java
+++ Candidate/src/main/java/com/esprit/microservice/candidateservice/CandidatRestAPI.java
@@ -1,17 +1,15 @@
 package com.esprit.microservice.candidateservice;
 
+import com.esprit.microservice.candidateservice.Candidat;
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/candidats")
@@ -25,19 +23,35 @@ public class CandidatRestAPI {
 		return title;
 	}
 	@PostMapping
+	@RequestMapping(value = "/user")
 	@ResponseStatus(HttpStatus.CREATED)
-	public ResponseEntity<Candidat> createCandidat(@RequestBody Candidat candidat) {
-		return new ResponseEntity<>(candidatService.addCandidat(candidat), HttpStatus.OK);
+	public ResponseEntity<Candidat> createCandidat(@RequestBody Candidat candidat, KeycloakAuthenticationToken auth) {
+		KeycloakPrincipal<KeycloakSecurityContext> principal = (KeycloakPrincipal<KeycloakSecurityContext>) auth.getPrincipal();
+		KeycloakSecurityContext context = principal.getKeycloakSecurityContext();
+		boolean hasUserRole = context.getToken().getRealmAccess().isUserInRole("user");
+
+		if (hasUserRole) {
+			return new ResponseEntity<>(candidatService.addCandidat(candidat), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
 	}
 	@PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<Candidat> updateCandidat(@PathVariable(value = "id") int id,
-    										@RequestBody Candidat candidat){
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Candidat> updateCandidat(@PathVariable(value = "id") int id,
+												   @RequestBody Candidat candidat){
 		return new ResponseEntity<>(candidatService.updateCandidat(id, candidat), HttpStatus.OK);
 	}
-	@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> deleteCandidat(@PathVariable(value = "id") int id){
-		return new ResponseEntity<>(candidatService.deleteCandidat(id), HttpStatus.OK);
+	@DeleteMapping(value = "/admin/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<String> deleteCandidat(@PathVariable(value = "id") int id, KeycloakAuthenticationToken auth){
+		KeycloakPrincipal<KeycloakSecurityContext> principal = (KeycloakPrincipal<KeycloakSecurityContext>) auth.getPrincipal();
+		KeycloakSecurityContext context = principal.getKeycloakSecurityContext();
+		boolean hasUserRole = context.getToken().getRealmAccess().isUserInRole("admin");
+		if (hasUserRole) {
+			return new ResponseEntity<>(candidatService.deleteCandidat(id), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
 	}
 }
---
+++ Candidate/src/main/java/com/esprit/microservice/candidateservice/KeycloakConfig.java
@@ -0,0 +1,60 @@
+package com.esprit.microservice.candidateservice;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.client.KeycloakClientRequestFactory;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    static Keycloak keycloak=null;
+     final static String serverUrl = "http://localhost:8180/auth";
+    public final static String realm = "jobBoardKeyCloak";
+    public final static String clientId = "candidat-service";
+     final static String clientSecret = "a6c9af2e-fda1-4b31-86bc-857595303274";
+    final static String userName = "abdou";
+    final static String password = "test123";
+
+    public KeycloakConfig() {
+    }
+@Bean
+    public static Keycloak getInstance() {
+        if (keycloak == null) {
+            keycloak = KeycloakBuilder.builder()
+                    .serverUrl(serverUrl)
+                    .realm(realm)
+                    .grantType(OAuth2Constants.PASSWORD)
+                    .username(userName)
+                    .password(password)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .resteasyClient(new ResteasyClientBuilder()
+                            .connectionPoolSize(10)
+                            .build())
+                    .build();
+        }
+    return keycloak;
+}
+
+
+
+
+
+
+
+}
+
+
+
